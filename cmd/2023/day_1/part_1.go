e42fbe64f31a102741f5388fc7d351b5e9e49a89
Kaique Caires <caires.kaique@gmail.com>
2023-12-01 08:48:42 -0300

    finished 2023_day_1_part_1

---
+++ cmd/2023/day_1/part_1.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	f, err := os.Open(pwd + "/cmd/2023/day_1/input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	total := 0
+
+	for scanner.Scan() {
+		characters := scanner.Text()
+		var twoChars [2]rune
+
+		for _, char := range characters {
+			if unicode.IsDigit(char) {
+				if twoChars[0] == 0 {
+					twoChars[0] = char
+				} else {
+					twoChars[1] = char
+				}
+			}
+		}
+
+		if twoChars[1] == 0 {
+			twoChars[1] = twoChars[0]
+		}
+
+		num, _ := strconv.Atoi(string(twoChars[0]) + string(twoChars[1]))
+
+		total += num
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(total)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kaiquecaires/advent-of-code
+
+go 1.20
